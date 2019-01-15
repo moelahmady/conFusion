d50d182f77b685fedcca8db8fb2d85f5ed041626
Mohamed Khaled <mo.elahmady@gmail.com>
2019-01-15 20:37:12 +0200

    Adding Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,15 +11,29 @@ import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 import  'hammerjs';
+
 import { MenuComponent } from './menu/menu.component';
-import { from } from 'rxjs';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
 import {DishService} from './services/dish.service';
+
+import {AppRoutingModule} from './app-routing/app-routing.module';
+
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -29,7 +43,8 @@ import {DishService} from './services/dish.service';
     FlexLayoutModule,
     MatGridListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    AppRoutingModule
   ],
   providers: [DishService],
   bootstrap: [AppComponent]
