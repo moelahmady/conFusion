6cc57daaeb40d3255a0e8e96c647db59b11ff9d7
Mohamed Khaled <mo.elahmady@gmail.com>
2019-01-16 21:37:10 +0200

    Template Forms Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,10 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import  'hammerjs';
@@ -50,7 +53,11 @@ import { MatDialogModule } from '@angular/material/dialog';
     MatCardModule,
     MatButtonModule,
     AppRoutingModule,
-    MatDialogModule
+    MatDialogModule,
+    MatFormFieldModule, 
+    MatInputModule,
+    MatCheckboxModule,
+    FormsModule
   ],
   providers: [DishService,PromotionService,LeaderService],
   bootstrap: [AppComponent],
